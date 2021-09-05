e04ff0237b1fe48181f0ab15d84238d4a317da81
Eugene Bystrov <evgeniy0ctreles@gmail.com>
2021-09-05 17:37:49 +0800

    # Refactor task 5

--- task5/js/script.js
+++ task5/js/script.js
@@ -1,35 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const int1 = document.getElementById('int1');
-    const int2 = document.getElementById('int2');
+    const input = document.getElementById('input');
 
     const button = document.getElementById('btn');
 
     const result = document.getElementById('result');
 
-    // АЛГОРИТМ (Евклида)
-    const gcd = (value1, value2) => {
-        value1 = Math.abs(value1);
-        value2 = Math.abs(value2);
+    // Позволяет произвести рассчет из строки
+    const calculateString = (str) => {
+        return (new Function('return ' + str))();
+    }
 
-        while (value1 != value2) {
-            if (value1 > value2) {
-                let tmp = value1;
-                value1 = value2;
-                value2 = tmp;
-            }
-            value2 = value2 - value1;
+    // АЛГОРИТМ
+    const findZeroDivider = (string) => {
+
+        const exprs = string.split('/');
+
+        for (let i = 0; i < exprs.length; i++) {
+            if (calculateString(exprs[i]) === 0) return true;
         }
 
-        return value1;
+        return false
     }
     // 
 
     button.addEventListener('click', () => {
-        if (!int1.value || !int2.value) {
-            result.innerHTML = "Введите оба числа!"
-            return;
-        }
-        
-        result.innerHTML = gcd(int1.value, int2.value);
+       if (!input.value) {
+           result.innerHTML = "Введите выражение!"
+           result.classList.remove('true');
+            result.classList.remove('false');
+           return;
+       }
+
+       console.log(findZeroDivider(input.value))
+       if (findZeroDivider(input.value)) {
+            result.innerHTML = "Обнаружено деление на ноль!"
+            result.classList.remove('true');
+            result.classList.add('false');
+       } else {
+            result.innerHTML = "Деления на ноль не обнаружено!"
+            result.classList.remove('false');
+            result.classList.add('true');
+       }
     })
 })
