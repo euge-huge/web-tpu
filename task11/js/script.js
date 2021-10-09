2315795f88a318732671c326e92b09cc97957cf5
Eugene Bystrov <evgeniy0ctreles@gmail.com>
2021-10-09 12:37:39 +0700

    # Added laba11

--- task11/js/script.js
+++ task11/js/script.js
@@ -1,36 +1,51 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const month = document.getElementById('input-month');
-    const year = document.getElementById('input-year');
+document.addEventListener("DOMContentLoaded", () => {
+  const month = document.getElementById("input-month");
+  const year = document.getElementById("input-year");
 
-    const button = document.getElementById('btn');
+  const button = document.getElementById("btn");
 
-    const result = document.getElementById('result');
+  const result = document.getElementById("result");
 
-    // АЛГОРИТМ 
-    const isContainFriday = (month, year) => {
-        
+  // АЛГОРИТМ
+  const isContainFriday = (month, year) => {
+    if (!/^([1-9]|1[012])$/.test(month))
+      throw new Error("Введите корректный месяц!");
+
+    if (!/^[0-9]+$/.test(year))
+      throw new Error("Год должен быть целым положительным числом!");
+
+    const date = new Date(year, month - 1, 13);
+
+    console.log(date);
+
+    if (date.getDay() == 5) {
+      return true;
+    } else return false;
+  };
+  //
+
+  button.addEventListener("click", () => {
+    if (!month.value || !year.value) {
+      result.innerHTML = "введите месяц и год!";
+      result.classList.remove("false");
+      result.classList.remove("true");
+      return;
     }
-    // 
 
-    console.log(new Date(-8.64e+7).getDay());
-
-    button.addEventListener('click', () => {
-        if (!month.value || !year.value) {
-            result.innerHTML = "введите месяц и год!"
-            result.classList.remove("false");
-            result.classList.remove("true");
-            return;
-        }
-
-        if (isAcceptedPassowrd(input.value)) {
-            result.innerHTML = "пароль прошел проверку!";
-            result.classList.remove("false");
-            result.classList.add("true");
-        } else {
-            result.innerHTML = "пароль не прошел проверку!";
-            result.classList.remove("true");
-            result.classList.add("false");
-        }
-        
-    })
-})
+    try {
+      if (isContainFriday(month.value, year.value)) {
+        result.innerHTML = "в этом месяце есть пятница 13-е =(";
+        result.classList.remove("true");
+        result.classList.add("false");
+      } else {
+        result.innerHTML = "в этом месяце нет пятницы 13-ого =)";
+        result.classList.remove("false");
+        result.classList.add("true");
+      }
+    } catch (e) {
+      result.innerHTML = e.message;
+      result.classList.remove("true");
+      result.classList.remove("false");
+    }
+  });
+});
